42917690078ed4df59b2b2f3ddb0ca4c9a660dec
thenuja1 <54266531+thenuja1@users.noreply.github.com>
2019-12-05 22:44:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Oppmuntring from './Oppmuntring'
+
+const App = () => {
+  return (
+    <div className="App">    
+    <Oppmuntring navn="Rick Astley" emne="Kunsten å synge rent" />
+  </div>
+  )
+}
+
+export default App
---
+++ src/Oppmuntring.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Oppmuntring = () => {
+return (
+    <div class="oppmuntring">
+        <h1>Kom igjen Rick Astley</h1>
+        <p>Kunsten å synge rent er ikke så vanskelig som du tror.</p>
+        <p>Du må bare øve</p>
+    </div>
+    )
+}
+
+export default Oppmuntring
